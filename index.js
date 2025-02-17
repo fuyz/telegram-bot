3059493c61a66f545448fdacccbd1351898bf5d0
fuyz <953958160@qq.com>
2025-02-17 11:10:41 +0800

    添加help和发送图片命令

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ bot.onText(/\/start/, (msg) => {
   bot.sendMessage(chatId, 'Hello! I am your Telegram bot.');
 });
 
+bot.onText(/\/help/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Here are the commands you can use:\n/start - Start the bot\n/help - Show help');
+});
+
+bot.onText(/\/photo/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendPhoto(chatId, 'https://www.ruanyifeng.com/blogimg/asset/2017/bg2017052701.png');
+});
+
 // 处理用户消息
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
