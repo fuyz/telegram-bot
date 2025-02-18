2d547270ccdac303104a593c8394a88e5ac6a264
fuyz <953958160@qq.com>
2025-02-18 09:48:55 +0800

    配置页面

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000; // 默认端口为 3000
-
+const path = require('path');
 // 首页路由
 app.get('/', (req, res) => {
-  res.send('Hello, 欢迎来到黑桃♠!');
+  // res.send('Hello, 欢迎来到黑桃♠!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // 启动服务器
