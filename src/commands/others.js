663f6a910bdb9dd8ff4fd2c9fe59c96406e0d7a0
fuyz <953958160@qq.com>
2025-02-21 17:11:04 +0800

    无效指令自动回复

--- src/commands/others.js
+++ src/commands/others.js
@@ -1,8 +1,36 @@
 module.exports = (bot) => {
+  const commands = [
+    { command: '/start', description: '开始使用机器人' },
+    { command: '/pay', description: '测试支付功能' },
+    { command: '/help', description: '查看帮助信息' },
+    { command: '/photo', description: '发送图片' },
+    { command: '/newlocation', description: '显示一个地址' },
+    { command: '/poll', description: '创建一个投票' },
+    { command: '/mycommand', description: '设置机器人命令列表' }
+  ];
+
+  // 无效指令自动回复
+  bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    // List of valid commands
+    const validCommands = commands.map(cmd => cmd.command);
+
+    // Check if the message is a command and if it's not in the valid commands list
+    if (text.startsWith('/') && !validCommands.includes(text)) {
+      bot.sendMessage(chatId, '对不起，我不理解该命令。输入 /help 查看可用命令列表。');
+    }
+  });
 
   bot.onText(/\/help/, (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Here are the commands you can use:\n/start - Start the bot\n/help - Show help');
+    let helpMessage = '可用命令列表:\n';
+    commands.forEach(cmd => {
+      helpMessage += `${cmd.command} - ${cmd.description}\n`;
+    });
+
+    bot.sendMessage(chatId, helpMessage);
   });
 
   bot.onText(/\/photo/, (msg) => {
@@ -28,15 +56,7 @@ module.exports = (bot) => {
 
   // 设置机器人命令列表
   bot.onText(/\/mycommand/, (msg) => {
-    bot.setMyCommands([
-      { command: '/start', description: '开始使用机器人' },
-      { command: '/pay', description: '测试支付功能' },
-      { command: '/help', description: '查看帮助信息' },
-      { command: '/photo', description: '发送图片' },
-      { command: '/newlocation', description: '显示一个地址' },
-      { command: '/poll', description: '创建一个投票' },
-      { command: '/mycommand', description: '设置机器人命令列表' }
-    ], { language_code: 'zh' });
+    bot.setMyCommands(commands, { language_code: 'zh' });
   });
 
   // bot.onText(/\/url/, (msg) => {
